bb954554b6b6049e94f8ef64948a0a7ad6cd1af1
Harsh Pandey <114309768+Harshp91@users.noreply.github.com>
2023-03-20 23:46:49 +0530

    Add files via upload

---
+++ array/setdifference.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void SetDifference(int a[], int m, int b[], int n)
+{
+    int c[m + n];
+    int i = 0, j = 0, k = 0;
+    while (i < m && j < n)
+    {
+        if (a[i] < b[j])
+        {
+            c[k++] = a[i++];
+        }
+        else if (a[i] > b[j])
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+    while (i < m)
+    {
+        c[k++] = a[i++];
+    }
+    for (int i = 0; i < k; i++)
+        cout << c[i] << " ";
+    cout << endl;
+}
+int main()
+{
+    int n, m;
+    cout<<"enter the both array size"<<endl;
+    cin >> n >> m;
+    int a[n], b[m];
+
+    cout<<"enter the both arrays"<<endl;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+        
+    cout << "Array After Set Difference (A-B) :\n";
+    SetDifference(a, n, b, m);
+    cout << "Array After Set Difference (B-A) :\n";
+    SetDifference(b, m, a, n);
+ return 0;
+}
+
